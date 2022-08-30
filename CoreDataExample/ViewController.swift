e92b6d1c8edb41448653b157c9071b5eaed2af6f
KolyaVepryk <mvepryk20@gmail.com>
2022-08-30 16:03:02 +0300

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by FREESKIER on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
