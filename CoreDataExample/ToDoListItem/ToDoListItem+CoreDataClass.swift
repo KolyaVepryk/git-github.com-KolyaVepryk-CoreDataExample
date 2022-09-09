82165da8459535b56b92c594c5cc07ba6b4d989f
KolyaVepryk <mvepryk20@gmail.com>
2022-09-09 15:38:56 +0300

    Created CoreDataExample App

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by FREESKIER on 30.08.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
